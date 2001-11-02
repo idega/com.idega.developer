75e411e4fad477de441c1d64601a4a6ff2ac151b
Aron Birkir Gudmundsson <aron@idega.com>
2001-11-02 03:21:17 +0000

    no message

--- src/java/com/idega/development/presentation/LocaleSetter.java
+++ src/java/com/idega/development/presentation/LocaleSetter.java
@@ -42,19 +42,24 @@ public class LocaleSetter extends PresentationObjectContainer {
       if(iwc.getParameter("save")!= null)
         save(iwc);
 
+			Locale defLocale = iwc.getApplicationSettings().getDefaultLocale();
+			ICLocale icDefLocale = ICLocaleBusiness.getICLocale(defLocale);
+
       Form form = new Form();
       Table T = new Table();
       T.add(IWDeveloper.getText("Use"),1,1);
       T.add(IWDeveloper.getText("Country"),2,1);
       T.add(IWDeveloper.getText("Language"),3,1);
+			T.add(IWDeveloper.getText("Region"),4,1);
+			T.add(IWDeveloper.getText("Default"),5,1);
 
       count = 1;
-      addToTable(T,ICLocaleBusiness.listOfLocales(true));
+      addToTable(T,ICLocaleBusiness.listOfLocales(true),icDefLocale);
       SubmitButton save = new SubmitButton("save","Save");
       count++;
       T.add(save,1,count);
       count++;
-      addToTable(T,ICLocaleBusiness.listOfLocales(false));
+      addToTable(T,ICLocaleBusiness.listOfLocales(false),null);
       T.add(new HiddenInput("loc_count",String.valueOf(count)));
       T.setCellpadding(2);
       //T.setBorder(1);
@@ -62,9 +67,10 @@ public class LocaleSetter extends PresentationObjectContainer {
       add(form);
   }
 
-  private void addToTable(Table T,List listOfLocales){
+  private void addToTable(Table T,List listOfLocales,ICLocale defLocale){
     if(listOfLocales != null){
       CheckBox chk;
+			RadioButton rb;
       ICLocale icLocale;
       Locale javaLocale;
       Iterator I = listOfLocales.iterator();
@@ -77,13 +83,20 @@ public class LocaleSetter extends PresentationObjectContainer {
         T.add(chk,1,count);
         T.add(IWDeveloper.getText(javaLocale.getDisplayCountry()),2,count);
         T.add(IWDeveloper.getText(javaLocale.getDisplayLanguage()),3,count);
-
+				T.add(IWDeveloper.getText(javaLocale.getDisplayVariant()),4,count);
+				if(defLocale != null && icLocale.getInUse() ){
+					rb = new RadioButton("default_locale",icLocale.getName());
+					T.add(rb,5,count);
+					if( defLocale.getID() == icLocale.getID() )
+					  rb.setSelected();
+				}
       }
     }
   }
 
   private void save( IWContext iwc){
     String sCount = iwc.getParameter("loc_count");
+
     if(sCount != null){
       java.util.Vector V = new java.util.Vector();
       int count = Integer.parseInt(sCount);
@@ -95,6 +108,10 @@ public class LocaleSetter extends PresentationObjectContainer {
         }
       }
       ICLocaleBusiness.makeLocalesInUse(V);
+
+			String sDefLocale = iwc.getParameter("default_locale");
+			if(sDefLocale != null )
+			  iwc.getApplicationSettings().setDefaultLocale(ICLocaleBusiness.getLocaleFromLocaleString(sDefLocale));
     }
   }
 
