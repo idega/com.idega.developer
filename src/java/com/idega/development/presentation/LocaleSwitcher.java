8680001dadc4d48f8bf5077492947cb90d965c31
Aron Birkir Gudmundsson <aron@idega.com>
2002-02-27 13:54:05 +0000

    no message

--- src/java/com/idega/development/presentation/LocaleSwitcher.java
+++ src/java/com/idega/development/presentation/LocaleSwitcher.java
@@ -11,7 +11,10 @@ import java.util.Locale;
 import java.util.List;
 import java.util.Iterator;
 import java.util.Enumeration;
+import java.util.Map;
+import java.util.HashMap;
 import com.idega.util.LocaleUtil;
+import com.idega.core.localisation.business.ICLocaleBusiness;
 
 /**
  * Title:        idega Framework
@@ -22,51 +25,52 @@ import com.idega.util.LocaleUtil;
  * @version 1.0
  */
 
-public class LocaleSwitcher extends PresentationObjectContainer {
+public class LocaleSwitcher extends com.idega.idegaweb.presentation.LocaleChanger {
 
 
-  public static String localesParameter="iw_localeswitcher_locale";
-
-  private static String action = "iw_localeswitcher_sub_action";
 
 
-  public LocaleSwitcher(){
+  public void make(IWContext iwc){
+    if(showLinks)
+      doLinkView(iwc);
+    else
+      doDeveloperView(iwc);
+
   }
 
-  public void main(IWContext iwc){
-      add(IWDeveloper.getTitleTable(this.getClass()));
+  private void doDeveloperView(IWContext iwc){
+    add(IWDeveloper.getTitleTable(this.getClass()));
+    IWMainApplication iwma = iwc.getApplication();
 
-      IWMainApplication iwma = iwc.getApplication();
+    DropdownMenu localesDrop = Localizer.getAvailableLocalesDropdown(iwma,localesParameter);
+    localesDrop.keepStatusOnAction();
+    localesDrop.setToSubmit();
+    localesDrop.setSelectedElement(iwc.getCurrentLocale().toString());
 
-      DropdownMenu localesDrop = Localizer.getAvailableLocalesDropdown(iwma,localesParameter);
-      localesDrop.keepStatusOnAction();
-      localesDrop.setToSubmit();
-      localesDrop.setSelectedElement(iwc.getCurrentLocale().toString());
+    Form form = new Form();
+    form.maintainParameter(IWDeveloper.actionParameter);
+    add(form);
+    form.add(IWDeveloper.getText("Select language:&nbsp;&nbsp;"));
+    form.add(localesDrop);
 
-      Form form = new Form();
-      form.maintainParameter(IWDeveloper.actionParameter);
-      add(form);
-      form.add(IWDeveloper.getText("Select language:&nbsp;&nbsp;"));
-      form.add(localesDrop);
+    Enumeration enum = iwc.getParameterNames();
+    while (enum.hasMoreElements()) {
+      form.maintainParameter((String)enum.nextElement());
+    }
 
-      Enumeration enum = iwc.getParameterNames();
-      while (enum.hasMoreElements()) {
-        form.maintainParameter((String)enum.nextElement());
-      }
+    doBusiness(iwc);
 
-      doBusiness(iwc);
-
-      add(IWDeveloper.getText("Current Locale:&nbsp;&nbsp;"));
-      add(iwc.getCurrentLocale().getDisplayName()+" ("+iwc.getCurrentLocale().toString()+")");
+    add(IWDeveloper.getText("Current Locale:&nbsp;&nbsp;"));
+    add(iwc.getCurrentLocale().getDisplayName()+" ("+iwc.getCurrentLocale().toString()+")");
   }
 
   private void doBusiness(IWContext iwc){
-      String localeValue = iwc.getParameter(localesParameter);
-      if(localeValue!=null){
-        Locale locale = LocaleUtil.getLocale(localeValue);
-        if(locale!=null){
-          iwc.setCurrentLocale(locale);
-        }
+    String localeValue = iwc.getParameter(localesParameter);
+    if(localeValue!=null){
+      Locale locale = LocaleUtil.getLocale(localeValue);
+      if(locale!=null){
+        iwc.setCurrentLocale(locale);
       }
+    }
   }
 }
